1977ed737cd2ddcbb3520382308f47ecbd79d697
luokery <cationzx_lijun@163.com>
2021-02-07 00:39:04 +0800

    初始化
    初始化

---
+++ src/test/java/org/demo/jenkins/springboot01/Springboot01ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.demo.jenkins.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
